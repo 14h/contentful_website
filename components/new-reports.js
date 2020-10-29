7af304dc2a7514a9c881cadd6643e349272b8b81
Abdallah Kawji <abdallah@styla.com>
2020-10-29 12:51:54 +0100

    update css

--- components/new-reports.js
+++ components/new-reports.js
@@ -10,7 +10,7 @@ export default function NewReports() {
                 The GDS Special Edition on COVID-19 was developed as part of a global effort to better understand the impact of the pandemic on people’s lives with a specific focus on the use of alcohol and other drugs, mental health and relationships. Over 55,000 people took part. The survey ran for 7 weeks in (May and - June 2020).
             </div>
             <div className='flex flex-col md:flex-row md:justify-center align-center max-w-full md:max-w-screen-lg mx-auto'>
-                <div className='relative h-56 w-56 bg-accent-6 m-3 mx-auto text-center md:m-12 rounded-lg p-3 pt-8 text-xl hover:bg-accent-3 duration-200 transition-colors cursor-pointer'>
+                <div className='relative h-56 w-56 bg-accent-10 border-rounder-accent-6 m-3 mx-auto text-center md:m-12 rounded-lg p-3 pt-8 text-xl hover:bg-accent-3 duration-200 transition-colors cursor-pointer'>
                     <div>The GDS Covid 19 Global Key Findings Report</div>
                     <div className='absolute bottom-0 left-0 right-0 w-full text-center text-accent-4 text-xl font-bold italic'>VIEW REPORT</div>
                 </div>
